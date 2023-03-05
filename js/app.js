7c5e7698fb690131bc30c6a9f191c6a306669a49
Aditta Chakraborty <adittacse@gmail.com>
2023-03-05 19:18:39 +0600

    temperature and condition added

--- js/app.js
+++ js/app.js
@@ -9,14 +9,24 @@ const loadTemperarture = city => {
 }
 
 const displayTemperature = data => {
-    const temperature = document.getElementById("temperature");
-    temperature.innerText = data.main.temp;
+    setInnerTextById("temperature", data.main.temp);
+    setInnerTextById("condition", data.weather[0].main);
+}
+
+const setInnerTextById = (id, text) => {
+    const temperature = document.getElementById(id);
+    temperature.innerText = text;
 }
 
 document.getElementById("btn-search").addEventListener("click", function() {
+    // get city
     const searchField = document.getElementById("search-field");
     const city = searchField.value;
     loadTemperarture(city);
+    
+    // set city
+    document.getElementById("city").innerText = city.charAt(0).toUpperCase() + city.slice(1);
+    searchField.value = "";
 });
 
 loadTemperarture("dhaka");
